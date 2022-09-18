a3d587f6c8dd31a2a33a3a2cf8b636404d8a52fd
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-09-18 14:35:07 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { debounce } from 'debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
